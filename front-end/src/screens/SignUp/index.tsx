6dc11225cc3da557452cf33e1078a844c058b138
Raul Val <raulval@outlook.com>
2022-08-24 15:36:21 -0300

    feat: integrates sign up page with api

--- front-end/src/screens/SignUp/index.tsx
+++ front-end/src/screens/SignUp/index.tsx
@@ -1,16 +1,44 @@
 import { useNavigation } from "@react-navigation/native";
-import { ScrollView } from "react-native";
-import Form from "../../components/Form";
-import Slogan from "../../components/Slogan";
-import user from "../../services/user";
-import { IBodyAuth, Nav } from "../../shared/interfaces";
+import Form from "components/Form";
+import Slogan from "components/Slogan";
+import { useState } from "react";
+import { ActivityIndicator, Alert, ScrollView, View } from "react-native";
+import user from "services/user";
+import { IBodyAuth, Nav } from "shared/interfaces";
 import { SignUpContainer } from "./styles";
 
 const SignUp = () => {
   const { navigate } = useNavigation<Nav>();
   const { signup } = user();
+  const [loading, setLoading] = useState<boolean>(false);
 
-  const handleSubmit = async ({ name, email, password }: IBodyAuth) => {};
+  const handleSubmit = async ({ name, email, password }: IBodyAuth) => {
+    try {
+      setLoading(true);
+      const response = await signup({ name, email, password });
+      Alert.alert("User created successfully");
+      navigate("Login");
+    } catch (error: any) {
+      if (error.response.data.message) {
+        Alert.alert(error.response.data.message);
+      } else if (error.response.data.error.message) {
+        Alert.alert(error.response.data.error.message);
+      } else {
+        Alert.alert("Something went wrong");
+        console.error(error);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
 
   const onPressBack = () => {
     navigate("Login");
