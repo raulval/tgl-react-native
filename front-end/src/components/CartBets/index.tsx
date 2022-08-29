d8eac19bd3b4a1937ef6e873c9849d799610c231
Raul Val <raulval@outlook.com>
2022-08-29 19:23:46 -0300

    feat: creates component CartBets

---
+++ front-end/src/components/CartBets/index.tsx
@@ -0,0 +1,47 @@
+import {
+  BetGameType,
+  BetNumbers,
+  BetPrice,
+  BetsContainer,
+  BetsWrapper,
+  DeleteBetButton,
+  DeleteBetContainer,
+  Separator,
+  TypePriceWrapper,
+} from "./styles";
+
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { ICartBets } from "shared/interfaces";
+import { currencyFormat } from "shared/utils";
+import Theme from "styles/theme";
+
+interface BetsProps {
+  data: ICartBets;
+  onDelete: () => void;
+}
+
+const CartBets = (props: BetsProps) => {
+  return (
+    <BetsContainer>
+      <DeleteBetContainer>
+        <DeleteBetButton onPress={props.onDelete}>
+          <Ionicons
+            name="trash-outline"
+            size={25}
+            color={Theme.colors.primary.text}
+          />
+        </DeleteBetButton>
+      </DeleteBetContainer>
+      <Separator color={props.data.color} />
+      <BetsWrapper>
+        <BetNumbers>{props.data.numbers.toString()}</BetNumbers>
+        <TypePriceWrapper>
+          <BetGameType color={props.data.color}>{props.data.type}</BetGameType>
+          <BetPrice>R$ {currencyFormat(props.data.price)}</BetPrice>
+        </TypePriceWrapper>
+      </BetsWrapper>
+    </BetsContainer>
+  );
+};
+
+export default CartBets;
---
+++ front-end/src/components/CartBets/styles.ts
@@ -0,0 +1,66 @@
+import styled from "styled-components/native";
+
+interface ColorProps {
+  color: string;
+}
+
+export const BetsContainer = styled.View`
+  width: 100%;
+  flex-direction: row;
+  margin-bottom: 35px;
+  margin-left: 17px;
+  justify-content: center;
+`;
+
+export const DeleteBetContainer = styled.View`
+  align-items: center;
+  justify-content: center;
+`;
+
+export const DeleteBetButton = styled.TouchableOpacity`
+  color: #888888;
+  margin-right: 14px;
+`;
+
+export const DeleteBetButtonText = styled.Text``;
+
+export const Separator = styled.View`
+  min-width: 4px;
+  min-height: 60px;
+  background-color: ${(props: ColorProps) => props.color};
+  border-radius: 100px;
+  margin-right: 20px;
+`;
+
+export const BetsWrapper = styled.View`
+  flex-direction: column;
+  width: 240px;
+`;
+
+export const BetNumbers = styled.Text`
+  width: 165px;
+  font-family: ${(props) => props.theme.fonts.title600Italic};
+  font-size: 15px;
+  color: #868686;
+  margin-bottom: 6px;
+
+  /* overflow-wrap: break-word; */
+  /* min-height: min-content; */
+`;
+
+export const TypePriceWrapper = styled.View`
+  margin-top: 15px;
+`;
+
+export const BetGameType = styled.Text`
+  font-family: ${(props) => props.theme.fonts.title600Italic};
+  font-size: 16px;
+  color: ${(props: ColorProps) => props.color};
+  margin-right: 14px;
+`;
+
+export const BetPrice: any = styled.Text`
+  font-family: ${(props) => props.theme.fonts.text400};
+  font-size: 16px;
+  color: #868686;
+`;
