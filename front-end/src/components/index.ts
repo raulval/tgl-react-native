752f5a6875cf0d337040fb9c8020405b33619965
Raul Val <raulval@outlook.com>
2022-08-30 11:57:48 -0300

    feat: adds indexing to components and screens

---
+++ front-end/src/components/index.ts
@@ -0,0 +1,21 @@
+import AddButton from "./AddButton";
+import BetNumbers from "./BetNumbers";
+import Bets from "./Bets";
+import Cart from "./Cart";
+import CartBets from "./CartBets";
+import Form from "./Form";
+import GameButton from "./GameButton";
+import HeaderCartButton from "./HeaderCartButton";
+import Slogan from "./Slogan";
+
+export {
+  AddButton,
+  BetNumbers,
+  Bets,
+  Cart,
+  CartBets,
+  Form,
+  GameButton,
+  HeaderCartButton,
+  Slogan,
+};
---
+++ front-end/src/screens/index.ts
@@ -0,0 +1,9 @@
+import Account from "./Account";
+import Bet from "./Bet";
+import Home from "./Home";
+import Login from "./Login";
+import NewPassword from "./NewPassword";
+import Reset from "./Reset";
+import SignUp from "./SignUp";
+
+export { Account, Bet, Home, Login, NewPassword, Reset, SignUp };
